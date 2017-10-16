81bc190c74004fb521d208c79017dae2ebaae881
chris-campbell <christopher.campbell@outlook.com>
2017-10-16 22:40:20 +0000

    added course listing index for students and link to listing in to nav bar

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root 'static_pages#index'
   devise_for :users
-    
+  
+  resources :courses, only: [:index, :show]
   namespace :instructor do
     resources :courses, only: [:new, :create, :show]
   end
