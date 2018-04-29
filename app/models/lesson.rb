67660bbce57928ef389a28f0b3ce9a4a474a7e03
Kris Camp <kris.camp@outlook.com>
2018-04-29 21:12:05 +0000

    added the video.path to the Vitdeotime function in duration method

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -15,7 +15,7 @@ class Lesson < ApplicationRecord
     end
     
     def duration(video)
-      time = Videotime.get_video_time(video)
+      time = Videotime.get_video_time(video.path)
       return "#{ time / 60 }" + ":" + "#{ time % 60 }"
     end
 
